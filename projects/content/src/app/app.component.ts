b148b2ea496dc42d820e0a1938b711f5da0839b3
javierReyVargas <javierre10va@gmail.com>
2023-11-17 14:06:15 -0500

    call LookAndFeelRepository on ngOnInit to get data and send it to applyThem function

--- projects/content/src/app/app.component.ts
+++ projects/content/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LookAndFeelRepository } from './core/repositories/look-and-feel.repository';
+import { LookAndFeelModel } from './core/models/lookAndFeel.model';
+import { applyTheme } from './styleLoad';
+import { LookAndFeelDTO } from './infraestructure/DTO/look-and-feel.dto';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private lookAndFeelRepository: LookAndFeelRepository) { }
+
+  ngOnInit(): void {
+    this.lookAndFeelRepository.getLookAndFeel().subscribe(
+      {
+        next: (lookAndFeel: LookAndFeelDTO|LookAndFeelModel) => {
+          applyTheme(lookAndFeel as LookAndFeelModel);
+        },
+        error: (error) => {
+          console.log('error: ', error);
+        }
+      }
+    );
+  }
 
 }
