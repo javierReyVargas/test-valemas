f4630749873618067fc8e5eb180fc7eb48b67396
javierReyVargas <javierre10va@gmail.com>
2023-11-18 14:40:49 -0500

    add new component for header

--- projects/content/src/app/module/main/main.module.ts
+++ projects/content/src/app/module/main/main.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
+import { HeaderComponent } from '../components/header/header.component';
 
 
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [MainComponent, HeaderComponent],
   imports: [
     CommonModule,
     MainRoutingModule
