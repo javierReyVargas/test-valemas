56bba7bfc4ca03cc9848f8aa8e98751c7f01bc40
javierReyVargas <javierre10va@gmail.com>
2023-11-18 16:19:45 -0500

    add new component for menu

--- projects/content/src/app/module/main/main.module.ts
+++ projects/content/src/app/module/main/main.module.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
+
+//new components
 import { HeaderComponent } from '../components/header/header.component';
+import { MenuComponent } from '../components/menu/menu.component';
 
 
 @NgModule({
-  declarations: [MainComponent, HeaderComponent],
+  declarations: [
+    MainComponent,
+    HeaderComponent,
+    MenuComponent
+  ],
   imports: [
     CommonModule,
     MainRoutingModule
