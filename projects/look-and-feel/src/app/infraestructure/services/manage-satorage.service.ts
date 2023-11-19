9aa9526cf66b1b0ac38245d622340151ca80d4eb
javierReyVargas <javierre10va@gmail.com>
2023-11-19 12:19:08 -0500

    add service to manage data session storage in to LookAndFeel project

---
+++ projects/look-and-feel/src/app/infraestructure/services/manage-satorage.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { LookAndFeelDTO } from '../DTO/look-and-feel.dto';
+const LOOK_AND_FEEL_KEY = 'look-and-feel';
+@Injectable({
+  providedIn: 'root'
+})
+export class ManageStorageService {
+
+  setItem(lookAndFeel: LookAndFeelDTO) {
+    sessionStorage.setItem(LOOK_AND_FEEL_KEY, JSON.stringify(lookAndFeel));
+  }
+
+  getItem() {
+    const item = sessionStorage.getItem(LOOK_AND_FEEL_KEY);
+    if (item) {
+      return JSON.parse(item);
+    }
+    return null;
+  }
+}
