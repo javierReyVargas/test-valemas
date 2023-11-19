7db91975cf4f04ed412cfc486544c70283c3404b
javierReyVargas <javierre10va@gmail.com>
2023-11-19 12:22:13 -0500

    add component to module

--- projects/look-and-feel/src/app/modules/look-and-feel/look-and-feel.module.ts
+++ projects/look-and-feel/src/app/modules/look-and-feel/look-and-feel.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { LookAndFeelRoutingModule } from './look-and-feel-routing.module';
 import { SystemDesignComponent } from '../../modules/look-and-feel/system-design/system-design.component';
+import { ChangeColorComponent } from './components/change-color/change-color.component';
 
 
 @NgModule({
-  declarations: [SystemDesignComponent],
+  declarations: [SystemDesignComponent, ChangeColorComponent],
   imports: [
     CommonModule,
     LookAndFeelRoutingModule,
